39e9a9b09e2edf2147743d1a9227d5d85174a1aa
Vincent Esche <regexident@gmail.com>
2015-02-01 18:24:12 +0100

    Added minimal demo project.

---
+++ AppSandboxFileAccessDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AppSandboxFileAccessDemo
+//
+//  Created by Definite Loop on 01/02/15.
+//  Copyright (c) 2015 Leigh McCulloch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
